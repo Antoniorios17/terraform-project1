1e272df652f6af5e33879c21ac0294d2b7fd74a8
antonio <antonio@gmail.com>
2022-10-22 13:40:21 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  type = string
+
+}
+
+variable "name" {
+  type = string
+  
+
+}
+variable "vpc_cidr" {
+  type = string 
+
+}
+variable "aws_access_key" {
+  type = string 
+
+}
+variable "aws_secret_key" {
+  type = string 
+
+}
